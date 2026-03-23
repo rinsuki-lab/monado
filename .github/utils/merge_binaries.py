88565d39ac7b5d5ae805960328e003f5730b257c
rinsuki <428rinsuki+git@gmail.com>
2026-03-23 19:53:10 +0900

    fix ci

--- .github/utils/merge_binaries.py
+++ .github/utils/merge_binaries.py
@@ -1,32 +1,53 @@
 import subprocess
-import glob
 import sys
 import shutil
+import os.path
 
 SRC_DIR = sys.argv[1]
 DST_DIR = sys.argv[2]
 
 binaries = dict[str, list[str]]()
 
+def walker(folder: str):
+    with os.scandir(folder) as it:
+        for entry in it:
+            path = os.path.join(folder, entry.name)
+            name = os.path.relpath(path, SRC_DIR)
+            if "/" not in name:
+                if entry.is_dir():
+                    walker(path)
+                continue
+            name = name[name.index("/") + 1:]
+            if entry.is_symlink():
+                binaries[name] = [path]
+            elif entry.is_dir():
+                walker(path)
+            else:
+                if name not in binaries:
+                    binaries[name] = [path]
+                else:
+                    binaries[name].append(path)
+
 def main():
-    for path in glob.iglob(SRC_DIR + "/*/*.dylib"):
-        name = path.split("/")[-1]
-        if name not in binaries:
-            binaries[name] = [path]
-        else:
-            binaries[name].append(path)
+    walker(SRC_DIR)
 
     for name, paths in binaries.items():
-        if len(paths) == 1:
-            shutil.copyfile(paths[0], f"{DST_DIR}/{name}")
+        print(name, paths, os.path.islink(paths[0]))
+        os.makedirs(os.path.dirname(f"{DST_DIR}/{name}"), exist_ok=True)
+        if len(paths) == 1 or os.path.islink(paths[0]):
+            shutil.copyfile(paths[0], f"{DST_DIR}/{name}", follow_symlinks=False)
         else:
-            subprocess.run([
-                "lipo",
-                "-create",
-                *paths,
-                "-output",
-                f"{DST_DIR}/{name}"
-            ])
+            with open(paths[0], "rb") as f:
+                if f.read(4) != b"\xcf\xfa\xed\xfe":
+                    shutil.copyfile(paths[0], f"{DST_DIR}/{name}")
+                else:
+                    subprocess.run([
+                        "lipo",
+                        "-create",
+                        *paths,
+                        "-output",
+                        f"{DST_DIR}/{name}"
+                    ], check=True)
 
 if __name__ == "__main__":
     main()
